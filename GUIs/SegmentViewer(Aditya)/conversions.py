809db917b6548414e20a9d58fbb1b715094ceee1
D0NMEGA <Don.mega306@gmail.com>
2026-02-09 00:57:07 -0600

    Immaculate Stacked mode, bug fixes, color stuff, smoothing, overall improvement

--- GUIs/SegmentViewer(Aditya)/conversions.py
+++ GUIs/SegmentViewer(Aditya)/conversions.py
@@ -5,10 +5,18 @@ import mne
 def get_pkl_array(path):
   with open(path, 'rb') as file:
     data = pickle.load(file)
-    print(data[1])
+    metadata = {}
+    if len(data) > 1 and isinstance(data[1], dict):
+      metadata = data[1]
+    elif len(data) > 1:
+      print(f"PKL metadata (non-dict): {data[1]}")
     data = np.array(data[0])
-  return data
+  return data, metadata
 
 def get_gdf_array(path):
-  raw = mne.io.read_raw_gdf(path)
-  return raw.get_data()
+  raw = mne.io.read_raw_gdf(path, preload=True)
+  metadata = {
+    'sfreq': raw.info['sfreq'],
+    'ch_names': raw.info['ch_names'],
+  }
+  return raw.get_data(), metadata
